d1fa672cc672d85914c14d2abe419b1cb8b80191
unknown <YY436@ccs.local>
2025-01-31 04:47:00 -0800

    Updated script.js somehow all indent was gone; re-added indent

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //Array to store todos
-
 let todos= [];
 
 //DOM Elements
@@ -9,44 +8,44 @@ const todoList = document.getElementById('todo-list');
 
 //Function to render tools
 function renderTodos() {
-todoList.innerHTML = ''; //Clear List
+    todoList.innerHTML = ''; //Clear List
 todos.forEach((todo, index) => {
-const li = document.createElement('li');
-li.className = 'todo-item';
-li.innerHTML = `
-<span>${todo}</span>
-<button onclick="editTodo(${index})">Edit</button>
-<button onclick="deleteTodo(${index})">Delete</button>
-`;
-todoList.appendChild(li);
-});
+    const li = document.createElement('li');
+    li.className = 'todo-item';
+    li.innerHTML = `
+        <span>${todo}</span>
+        <button onclick="editTodo(${index})">Edit</button>
+        <button onclick="deleteTodo(${index})">Delete</button>
+        `;
+    todoList.appendChild(li);
+    });
 }
 
 //Function to add a new todo
 function addTodo(event) {
-event.preventDefault(); //Prevent fron Submission
-const newTodo = todoInput.value.trim();
-if (newTodo) {
-todos.push(newTodo);
-todoInput.value = '';
-renderTodos();
-}
+    event.preventDefault(); //Prevent fron Submission
+    const newTodo = todoInput.value.trim();
+    if (newTodo) {
+        todos.push(newTodo);
+        todoInput.value = '';
+        renderTodos();
+    }
 }
 //Function to edit a TOdo
 function editTodo(index){
-const updatedTodo = prompt('Edit your Todo:', todos[index]);
-if (updatedTodo !== null){
-todos [index] = updatedTodo.trim();
-renderTodos();
-}
+    const updatedTodo = prompt('Edit your Todo:', todos[index]);
+    if (updatedTodo !== null){
+        todos [index] = updatedTodo.trim();
+        renderTodos();
+    }
 }
 
 //Function to delte a todo
 function deleteTodo(index){
-if (confirm('Are you sure you want to delete this todo?')) {
-todos.splice(index, 1);
-renderTodos();
-}
+    if (confirm('Are you sure you want to delete this todo?')) {
+        todos.splice(index, 1);
+        renderTodos();
+    }
 }
 
 //Event Listeners
